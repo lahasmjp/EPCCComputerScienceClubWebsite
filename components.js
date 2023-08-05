1dca71fc340c31bc44603dc2a998af8cb787cee9
lahMJP <124839307+lahasmjp@users.noreply.github.com>
2023-08-04 21:21:53 -0600

    Visual and Technical Improvements
    Added "components.js" to define custom elements to stop consistency errors with elements that are used repeatedly. Elements effected by this are: 
    	- Header
    	- Footer
    	- Social Media Plugs
    - Changed the splash screen image. The styling has also changed.
    - Removed the "E-Points" and "Contact Us" pages from the header navbar.
    - Archived "E-Points" page and made it inaccessible.
    - Removed "Contact Us" page and its associated files (RIP).
    - Added "Ambassadors / Contacts" page and made it accessible in the header navbar.
    - Added additional (and possibly temporary) stylesheet for "Ambassadors / Contacts" page.

---
+++ components.js
@@ -0,0 +1,44 @@
+class Header extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <header>
+        <img id="clublogo" src="./images/clublogo.png">
+        <nav id="headernav">
+            <li><a class="navlink" href="./index.html">Home</a></li>
+            <!--<li><a class="navlink" href="./epoints.html">E-Points</a></li>-->
+            <li><a class="navlink" href="./ambassadors.html">Ambassadors / Contacts</a></li>
+        </nav>
+        </header>
+        `;
+    }
+}
+class Footer extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer>
+            <p>El Paso County Community College District does not discriminate on the basis of race, color, national
+                origin, religion, gender, age, disability, veteran status, sexual orientation, or gender identity.</p>
+                <section>
+                    <img class="medIcon" src="./images/epcc.png">
+                    <img class="medIcon" src="./images/acm.png">
+                </section>
+        </footer>
+        `
+    }
+}
+class socialMedia extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <nav id="smNav">
+                <h4>Visit our Social Medias!</h4>
+                <a href="https://www.instagram.com/epcc_computer_science_club/?igshid=YmMyMTA2M2Y%3D"
+                    target="_blank" title="Follow us for pictures and updates on events!"><img src="./images/socialicons/insta.png" class="smIcon"></a>
+                <a href="https://discord.gg/jjzkRn6x3q" target="_blank" title="Join our community and connect with fellow majors!"><img src="./images/socialicons/discord.png"
+                        class="smIcon"></a>
+            </nav>
+        `
+    }
+}
+customElements.define('main-header', Header);
+customElements.define('main-footer', Footer);
+customElements.define('socialmedia-plugs', socialMedia);
